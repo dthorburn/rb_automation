e5bf7a0058d28f212cfdf93c6e96f0c5f9ec1565
Resurrect Bio <miles.thorburn@gmail.com>
2026-03-09 12:19:00 +0000

    updated iprscan with yml

--- interproscan/run_interproscan.sh
+++ interproscan/run_interproscan.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Used to generate the requisite iprscan input gff for the RB_Candidate_Genes pipeline for NLRs. 
+# Used to generate the requisite iprscan input gff for the RB_Candidate_Genes pipeline for NLRs.
 # No need to invoke the iprscan conda env with the PATH update
 #
 export PATH=/home/resurrect/conda/envs/iprscan/bin:$PATH
 
 IPR_DIR=/mnt/sda1/interproscan/interproscan-5.71-102.0
-DATA_DIR=/mnt/sdb1/candidate_search/crop_nlrs/soleracea/varoflay
-CPUS=16
+DATA_DIR=/mnt/sdb1/commercial_projects/corn_multiple/01_nlrs/CML277
+CPUS=30
 
 fasta=`ls -1 ${DATA_DIR}/*.fasta`
 sed -i "s/\*//g" ${fasta}
